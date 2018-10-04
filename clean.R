23b54dcc69f73121e4ed880babe0ef0360852eca
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:50:09 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,18 +1,16 @@
 
 clean_data <- as.data.frame("raw_data")
 
-clean_data <- function(){
-# reads the csv file with raw data
-dfStates <- read.csv("scprc_est_2017_18_pop_res.csv")
+f1 <- function(states){
 # removes the first row and the last row, with state = USA and state = Puerto rico commonwealth
 # and the first four columns SUMLEV REGION DIVISION STATE
-dfStates <- dfStates[,c(-1,-2,-3,-4)]
+dfStates <- dfStates[,-c(1:4)]
 dfStates <- dfStates[c(-1,-53),]
 
 #Using colnames() to replace the names of the columns in the dataframe.
 colnames(dfStates)[c(1,2,3,4)] <- c("stateName", "population", "popOver18", "percentOver18")
 #returns the clean dataframe
 
-  return(dfStates)
+  return(states)
 
 }
