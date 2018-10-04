f3543a0aec8a2e4057a3eb79bd448e1c341fe34f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 12:10:07 -0400

    Saving mergingDataframe.R

--- mergingDataframe.R
+++ mergingDataframe.R
@@ -3,7 +3,7 @@
 arrests <- USArrests
 
 #creating a column name for rows having states using rownames()
-rownames(clean_data) <- as.vector(clean_data[,"States"])
+rownames(clean_data) <- as.vector(clean_data[,"stateName"])
 
 #merging a merged dataframe with attributes from both dataframes
-mergeDataframe <- merge(clean_data, arrests, by = rownames("States") )
+mergeDataframe <- merge(clean_data, arrests, by = rownames("stateName") )
