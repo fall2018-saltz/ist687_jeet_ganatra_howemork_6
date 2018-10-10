d96f0786063e13c91ff84cbdef70429ad8eea554
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 12:43:35 -0400

    Saving mudersPerState.R

--- mudersPerState.R
+++ mudersPerState.R
@@ -2,6 +2,7 @@
 library("ggplot2")
 mergeDataframe$numMurders <- as.integer(mergeDataframe$population*mergeDataframe$Murder/100000)
 murdersPerState <- mergeDataframe[,c(2,10)]
+murdersPerState
 barChart1 <- ggplot(mergeDataframe, aes(x=stateName, y=numMurders))
 barChart1 <- barChart1 + geom_col()
 barChart1 <- barChart1 + ggtitle("Bar chart of murders per state")
