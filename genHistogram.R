7bd40ebf6a58de2a92f2be94ee323bdde0c60c0d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 14:38:53 -0400

    Saving genHistogram.R

--- genHistogram.R
+++ genHistogram.R
@@ -2,7 +2,7 @@
 #importing library for ggplot2
 library("ggplot2")
 #using ggplot() to generate a histogram with population on the x-axis from the mergeDataframe
-#dataset. We are using binwidth attribute to group the data into bins. Here, we are using 
+#dataset. We are using geom_histogram() function with binwidth attribute to group the data into bins. Here, we are using 
 #500,000 as binwidth because the population is in millions.
 myPlotPop <- ggplot(mergeDataframe, aes(x=population))
 myPlotPop <- myPlotPop + geom_histogram(binwidth = 500000)
@@ -24,12 +24,15 @@ myPlotPop2 <- myPlotPop2 + geom_histogram(binwidth=10)
 myPlotPop2 <- myPlotPop2 + ggtitle("Histogram of Assault")
 myPlotPop2
 
+#using ggplot() to generate a histogram with Rape on the x-axis from the mergeDataframe
+#dataset.
 myPlotPop3 <- ggplot(mergeDataframe, aes(x=Rape))
 myPlotPop3 <- myPlotPop3 + geom_histogram(binwidth=1)
 myPlotPop3 <- myPlotPop3 + ggtitle("Histogram of Rape")
 myPlotPop3
 
-
+#using ggplot() to generate a histogram with UrbanPop on the x-axis from the mergeDataframe
+#dataset.
 myPlotPop4 <- ggplot(mergeDataframe, aes(x=UrbanPop))
 myPlotPop4 <- myPlotPop4 + geom_histogram(binwidth=2)
 myPlotPop4 <- myPlotPop4 + ggtitle("Histogram of UrbanPop")
