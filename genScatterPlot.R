c3e9ab72fb9c0f255bd547ac6cbbd1a6481775a2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 13:51:45 -0400

    Saving genScatterPlot.R

--- genScatterPlot.R
+++ genScatterPlot.R
@@ -1,1 +1,6 @@
 
+library("ggplot2")
+scatterplot <- ggplot(mergeDataframe, aes(x= population, y =popOver18))
+scatterplot <- scatterplot + geom_point(aes(size = numMurders, color = numMurders))
+scatterplot < scatterplot + ggtitle("Scatter Plot")
+scatterplot
