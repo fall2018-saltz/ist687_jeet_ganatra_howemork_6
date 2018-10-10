8a3ff71e8fa95f6f444a923ed715ed2e0a2ea616
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 00:56:08 -0400

    Saving mudersPerState.R

--- mudersPerState.R
+++ mudersPerState.R
@@ -10,4 +10,5 @@ barChart1
 barChart2 <- ggplot(mergeDataframe, aes(x=stateName, y=numMurders))
 barChart2 <- barChart2 + geom_col()
 barChart2 <- barChart2 + ggtitle("Bar chart of murders per state")
+barChart2 <- barChart2 + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 barChart2
