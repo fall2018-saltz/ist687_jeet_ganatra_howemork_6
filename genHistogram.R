4a29bc1ff071f4ff7acafeffdc3edfc4befe9eb1
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 14:39:52 -0400

    Saving genHistogram.R

--- genHistogram.R
+++ genHistogram.R
@@ -10,8 +10,8 @@ myPlotPop <- myPlotPop + ggtitle("Histogram of Population")
 myPlotPop
 
 #using ggplot() to generate a histogram with murder on the x-axis from the mergeDataframe
-#dataset. We are using geom_histogram() function with binwidth attribute to group the data into bins. Here, we are using 
-1 as binwidth because the murder rate does not have large numbers.
+#dataset. We are using geom_histogram() function with binwidth attribute to group the data into
+#bins. Here, we are using 1 as binwidth because the murder rate does not have large numbers.
 myPlotPop1 <- ggplot(mergeDataframe, aes(x=Murder))
 myPlotPop1 <- myPlotPop1 + geom_histogram(binwidth=1)
 myPlotPop1 <- myPlotPop1 + ggtitle("Histogram of Murder")
