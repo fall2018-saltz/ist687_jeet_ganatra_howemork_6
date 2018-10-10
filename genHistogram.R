f00b2422bb2b8e9b6f022c4cccc6275b184f01ab
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 23:31:52 -0400

    Saving genHistogram.R

--- genHistogram.R
+++ genHistogram.R
@@ -5,7 +5,7 @@ myPlotPop <- myPlotPop + geom_histogram(binwidth = 500000)
 myPlotPop <- myPlotPop + ggtitle("Histogram of Population")
 myPlotPop
 
-myPlotPop1 <- ggplot(mergeDf, aes(x=Murder))
+myPlotPop1 <- ggplot(mergeDataframe, aes(x=Murder))
 myPlotPop1 <- myPlotPop1 + geom_histogram(binwidth=1)
 myPlotPop1 <- myPlotPop1 + ggtitle("Histogram of Murder")
 myPlotPop1
