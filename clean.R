c155d8d8f477ece2ed538df11faca057be7eac43
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 14:29:17 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -8,9 +8,7 @@ states <- states[c(-1,-53),]
 #Using colnames() to replace the names of the columns in the dataframe.
 colnames(states)[c(1,2,3,4)] <- c("stateName", "population", "popOver18", "percentOver18")
 #returns the clean dataframe
-
   return(states)
-
 }
 
 clean_data <- clean_data(raw_data)
