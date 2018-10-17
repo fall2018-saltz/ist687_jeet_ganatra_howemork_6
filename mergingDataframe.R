f953699489fab3fc7e985e5e15c3883efa861f0e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 13:58:01 -0400

    Saving mergingDataframe.R

--- mergingDataframe.R
+++ mergingDataframe.R
@@ -1,24 +1,10 @@
 
-cleandata2 <- USArrests
+#copying USArrests into a local variable
+arrests <- USArrests
 
+#creating a column name for rows having states using rownames()
+rownames(clean_data) <- as.vector(clean_data[,"stateName"])
 
-# save row names as a separate variable
-stateName <- as.vector(rownames(USArrests))
-stateName <- tolower(stateName)
-# add this column to the dataframe as a column names stateName
-cleandata2 <- cbind(cleandata2,stateName)
-
-# merge the dataframes based on the stateName column in both the dataframes
-# the merge function combines dataframes based on common columns
-# here it merges the dataframes by the common column stateName
-
-mergeDataframe <- merge(clean_data,cleandata2,by="stateName")
-
-#clean_data_merged
-statecenterx <- state.center$x
-statecentery <- state.center$y
-mergeDataframe <- cbind(mergeDataframe,state.area)
-mergeDataframe <- cbind(mergeDataframe,statecenterx)
-mergeDataframe <- cbind(mergeDataframe,statecentery)
-
-str(mergeDataframe)
+#merging 2 dataframes with attributes from both dataframes
+mergeDataframe <- merge(clean_data, arrests, by = "row.names")
+mergeDataframe
